2c85b9bb1422e69c9d876b567ec37a6436b28908
Chathura Hettiarachchi <chathura93@yahoo.com>
2023-09-14 15:42:09 +1200

    app update

---
+++ components/jobdetails/company/company.style.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../../../constants";
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: SIZES.medium,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    logoBox: {
+        width: 80,
+        height: 80,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF",
+        borderRadius: SIZES.large,
+        shadowColor: "black",
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 3,
+        backgroundColor: "white",
+    },
+    logoImage: {
+        width: "80%",
+        height: "80%",
+    },
+    jobTitleBox: {
+        marginTop: SIZES.small,
+    },
+    jobTitle: {
+        fontSize: SIZES.large,
+        color: COLORS.primary,
+        fontFamily: FONT.bold,
+        textAlign: "center",
+    },
+    companyInfoBox: {
+        marginTop: SIZES.small / 2,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    companyName: {
+        fontSize: SIZES.medium - 2,
+        color: COLORS.primary,
+        fontFamily: FONT.medium,
+    },
+    locationBox: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    locationImage: {
+        width: 14,
+        height: 14,
+        tintColor: COLORS.gray,
+    },
+    locationName: {
+        fontSize: SIZES.medium - 2,
+        color: COLORS.gray,
+        fontFamily: FONT.regular,
+        marginLeft: 2,
+    },
+});
+
+export default styles;
